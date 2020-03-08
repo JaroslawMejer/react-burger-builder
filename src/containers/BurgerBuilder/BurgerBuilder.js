77a11ebdb7d529eedeff1cb25644fa3566347fb3
Jaroslaw Mejer <jaroslaw.mejer@gmail.com>
2020-03-08 18:11:42 +0100

    Add checkout page & basic routing, temporary disabled firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -94,28 +94,30 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('You continue!');
-        this.setState({loading: true})
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Jaroslaw Mejer',
-                address: {
-                    street: 'Test 1',
-                    zipCode: '4316',
-                    country: 'Germany'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
-        }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({loading: false, purchasing: false})
-            })
-            .catch(error => {
-                this.setState({loading: false, purchasing: false})
-            }); //ending ".json" is required by Firebase - it creates a new node called "orders"
+        // this.setState({loading: true})
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Jaroslaw Mejer',
+        //         address: {
+        //             street: 'Test 1',
+        //             zipCode: '4316',
+        //             country: 'Germany'
+        //         },
+        //         email: 'test@test.com'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // }
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         this.setState({loading: false, purchasing: false})
+        //     })
+        //     .catch(error => {
+        //         this.setState({loading: false, purchasing: false})
+        //     }); //ending ".json" is required by Firebase - it creates a new node called "orders"
+
+        this.props.history.push('/checkout')
     };
 
     render() {
